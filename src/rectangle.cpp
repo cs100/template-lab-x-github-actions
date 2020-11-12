93e2b1cebe4eb0c5cf3aeef88001bce209ffcbc0
sumar001 <umar.saad1998@gmail.com>
2020-11-12 01:14:29 -0800

    changed rectangle.cpp to fail tests

---
+++ src/rectangle.cpp
@@ -0,0 +1,31 @@
+#include "../header/rectangle.hpp"
+
+Rectangle::Rectangle() {
+    this->width = 0;
+    this->height = 0;
+}
+
+Rectangle::Rectangle(int w, int h) {
+    this->width = w;
+    this->height = h;
+}
+
+void Rectangle::set_width(int w) {
+    this->width = w;
+}
+
+void Rectangle::set_height(int h) {
+    this->height = h;
+}
+
+int Rectangle::area() {
+    if (width <= 0 || height <= 0)
+	return -2;
+    return this->width * this->height * 2;
+}
+
+int Rectangle::perimeter() {
+    if (width <= 0 || height <= 0)
+	return -2;
+    return (this->width * 3) + (this->height * 3);
+}
