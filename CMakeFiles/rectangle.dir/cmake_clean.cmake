9fed190e2769708f78422b39eb847aa6c8f3e123
AnhQNguyen <aqnguyen1206@gmail.com>
2021-05-07 19:48:21 -0700

    test 2

---
+++ CMakeFiles/rectangle.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rectangle.dir/src/main.cpp.o"
+  "CMakeFiles/rectangle.dir/src/rectangle.cpp.o"
+  "rectangle"
+  "rectangle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rectangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
