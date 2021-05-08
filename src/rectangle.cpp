f0bbbccc662354df5ac493c6df8757fbc0e9bf93
AnhQNguyen <aqnguyen1206@gmail.com>
2021-05-07 19:42:02 -0700

    test

---
+++ src/rectangle.cpp
@@ -0,0 +1,33 @@
+#include "../header/rectangle.hpp"
+
+Rectangle::Rectangle() {}
+
+Rectangle::Rectangle(int w, int h) {
+	this->width = w;
+	this->height = h;
+
+}
+void Rectangle::set_width(int w) {
+    this->width = w;
+}
+
+void Rectangle::set_height(int h) {
+    this->height = h;
+}
+
+int Rectangle::get_width() {
+	return this->width;
+}
+
+int Rectangle::get_height() {
+	return this->height;
+
+}
+
+int Rectangle::area() {
+    return this->width * this->height;
+}
+
+int Rectangle::perimeter() {
+	return (this->width * 2 + this->height * 2);
+}
