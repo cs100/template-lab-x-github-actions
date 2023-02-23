fb0fbab500521625332f6e7ee9baa9c42b2a1f82
arrdyas <57843677+arrdyas@users.noreply.github.com>
2023-02-23 10:48:41 -0800

    Delete CMakeFiles directory

--- CMakeFiles/area_calculator.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for area_calculator.
-# This may be replaced when dependencies are built.
--- CMakeFiles/area_calculator.dir/src/rectangle.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/area_calculator.dir/src/rectangle.cpp.o: \
- /home/csmajs/asriv033/template-lab-x-github-actions/src/rectangle.cpp \
- /usr/include/stdc-predef.h \
- /home/csmajs/asriv033/template-lab-x-github-actions/src/../header/rectangle.hpp
