82e4833f9a8017f00b70dda73341668dc044a6a0
arrdyas <arrdyas@gmail.com>
2023-02-23 10:42:40 -0800

    Changed tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,7 +2,7 @@
 #include "gtest/gtest.h"
 #include "../header/rectangle.hpp"
 
-TEST(rectangleTEST, testConstructor){
+TEST(Constructor, testNormal){
 
 Rectangle* testRec = new Rectangle(4,4);
 
@@ -12,7 +12,23 @@ EXPECT_EQ(testRec->area(),16);
 
 }
 
-TEST(rectangleTEST , testPerimeter){
+TEST (Constructor, testZero){
+
+Rectangle* testRec = new Rectangle(0,0);
+
+EXPECT_EQ(testRec->area(), 0);
+
+}
+
+TEST(Constructor, testNeg){
+
+Rectangle* testRec;
+
+EXPECT_DEATH(new Rectangle(-1, -2), "Width/height cannot be less than 0");
+
+}
+
+TEST(Perimeter , testDiff){
 
 Rectangle* testRec = new Rectangle(2,3);
 
@@ -21,7 +37,24 @@ EXPECT_EQ(testRec->perimeter(), 10);
 
     
 }
-TEST(rectangleTEST, testArea){
+
+TEST(Perimeter, testZero){
+
+Rectangle* testRec = new Rectangle(0,0);
+
+EXPECT_EQ(testRec->perimeter(), 0);
+
+}
+
+TEST(Perimeter, testSame){
+
+Rectangle* testRec = new Rectangle(4, 4);
+
+EXPECT_EQ(testRec->perimeter(), 16);
+
+}
+
+TEST(Area, testDiff){
 
 Rectangle* testRec = new Rectangle(4,5);
 
@@ -30,4 +63,18 @@ EXPECT_EQ(testRec->area(),20);
 
 }
 
+TEST(Area, testZero){
 
+Rectangle* testRec = new Rectangle(0,0);
+
+EXPECT_EQ(testRec->area(), 0);
+
+}
+
+TEST(Area, testSame){
+
+Rectangle* testRec = new Rectangle(4,4);
+
+EXPECT_EQ(testRec->area(), 16);
+
+}
