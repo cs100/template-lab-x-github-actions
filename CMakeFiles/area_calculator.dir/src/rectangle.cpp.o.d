54e213918a7dd1ce3fde1a2c092fc80f3b89add7
arrdyas <57843677+arrdyas@users.noreply.github.com>
2023-02-23 10:47:23 -0800

    Delete CMakeFiles directory

--- CMakeFiles/area_calculator.dir/src/rectangle.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/area_calculator.dir/src/rectangle.cpp.o: \
- /data/home/csmajs/apadi089/template-lab-x-github-actions/src/rectangle.cpp \
- /usr/include/stdc-predef.h \
- /data/home/csmajs/apadi089/template-lab-x-github-actions/src/../header/rectangle.hpp
